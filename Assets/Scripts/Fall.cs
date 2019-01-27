dc946dde8bbc3be72ce1bb487a8586974c5af40a
Teo Lee <ttslee@users.noreply.github.com>
2019-01-26 22:21:15 -0800

    Music, Jump not working.

--- Assets/Scripts/Fall.cs
+++ Assets/Scripts/Fall.cs
@@ -1,14 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Fall : MonoBehaviour
 {
+    private Scene thisScene;
     void OnTriggerEnter2D(Collider2D Other)
     {
         if (Other.name == "Player")
         {
-            Application.LoadLevel(Application.loadedLevel);
+            thisScene = SceneManager.GetActiveScene();
+            if (thisScene.name == "03 Level 1")
+            {
+                GameObject.FindGameObjectWithTag("Level_L").GetComponent<MusicClass>().StopMusic();
+                Application.LoadLevel(Application.loadedLevel);
+            }
+            if(thisScene.name == "05 Level 2")
+            {
+                Application.LoadLevel(Application.loadedLevel);
+                GameObject.FindGameObjectWithTag("Kitchen_L").GetComponent<MusicClass>().StopMusic();
+            }
+            
         }
     }
 }
