1ce31fb1f5ce736975e2e186c08207a84caf59a4
Tron <43283544+Rob-Tarr@users.noreply.github.com>
2019-01-26 22:45:14 -0800

    Added Audio Killer

--- Assets/Scripts/Fall.cs
+++ Assets/Scripts/Fall.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class Fall : MonoBehaviour
 {
+
+    AudioSource myAudioSource;
+
+    private void Start()
+    {
+        myAudioSource = FindObjectOfType<AudioSource>();
+    }
+
     void OnTriggerEnter2D(Collider2D Other)
     {
         if (Other.name == "Player")
         {
+            myAudioSource.Stop();
+            Destroy(myAudioSource.gameObject);
             Application.LoadLevel(Application.loadedLevel);
         }
     }
