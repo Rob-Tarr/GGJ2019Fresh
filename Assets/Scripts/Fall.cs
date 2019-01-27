30a4a6c039d9d439b1930a66c98b53e32bf00794
Teo Lee <ttslee@users.noreply.github.com>
2019-01-26 22:32:36 -0800

    Fixed jumping

--- Assets/Scripts/Fall.cs
+++ Assets/Scripts/Fall.cs
@@ -1,27 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+//using UnityEngine.SceneManagement;
 
 public class Fall : MonoBehaviour
 {
-    private Scene thisScene;
+    //private Scene thisScene;
     void OnTriggerEnter2D(Collider2D Other)
     {
         if (Other.name == "Player")
         {
-            thisScene = SceneManager.GetActiveScene();
-            if (thisScene.name == "03 Level 1")
-            {
-                GameObject.FindGameObjectWithTag("Level_L").GetComponent<MusicClass>().StopMusic();
-                Application.LoadLevel(Application.loadedLevel);
-            }
-            if(thisScene.name == "05 Level 2")
-            {
-                Application.LoadLevel(Application.loadedLevel);
-                GameObject.FindGameObjectWithTag("Kitchen_L").GetComponent<MusicClass>().StopMusic();
-            }
-            
+            Application.LoadLevel(Application.loadedLevel);
+            //thisScene = SceneManager.GetActiveScene();
+            //if (thisScene.name == "03 Level 1")
+            //{
+            //    GameObject.FindGameObjectWithTag("Level_L").GetComponent<MusicClass>().StopMusic();
+            //    Application.LoadLevel(Application.loadedLevel);
+            //}
+            //if(thisScene.name == "05 Level 2")
+            //{
+            //    Application.LoadLevel(Application.loadedLevel);
+            //    GameObject.FindGameObjectWithTag("Kitchen_L").GetComponent<MusicClass>().StopMusic();
+            //}
+
         }
     }
 }
