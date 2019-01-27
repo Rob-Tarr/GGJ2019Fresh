1c1d6b2bb561306d085ea40db29652b5925fb33a
Teo Lee <ttslee@users.noreply.github.com>
2019-01-27 12:03:59 -0800

    Added Enemy Movement and Vacuum

---
+++ Assets/Scripts/Enemy_Movement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Movement : MonoBehaviour
+
+
+    
+{
+    public float speed = 5f;
+    bool MovingRight, MovingLeft;
+    private Transform Right, Left;
+    public Vector3 ADD;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        MovingRight = false;
+        MovingLeft = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        Right = gameObject.transform.GetChild(0);
+        Left = gameObject.transform.GetChild(1);
+        if (MovingLeft)
+        {
+            ADD= -new Vector3(Time.deltaTime * speed, 0f, 0f);
+            transform.position += ADD;
+            if (transform.position.x<Left.position.x) //moved past leftmost
+            {
+                MovingLeft = false;
+                MovingRight = true;
+                gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            }
+
+        }
+        else if (MovingRight)
+        {
+            ADD = new Vector3(Time.deltaTime * speed, 0f, 0f);
+            transform.position += ADD;
+            if (transform.position.x > Right.position.x) //moved past leftmost
+            {
+                MovingLeft = true;
+                MovingRight = false;
+                gameObject.GetComponent<SpriteRenderer>().flipX = false;
+
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/Hurtbox.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hurtbox : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D Other)
+    {
+        if (Other.name == "Player")
+        {
+            Destroy(gameObject);
+            Other.GetComponent<Player_Movement>().MyRB.velocity= new Vector2(Other.GetComponent<Player_Movement>().MyRB.velocity.x, Other.GetComponent<Player_Movement>().JumpSpeed/1.5f);//half jump
+        }
+    }
+}
